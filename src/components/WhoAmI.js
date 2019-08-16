548f1f0611c171ef0e9888744b39f0e297be7765
Pablo Suarez <pablo.suarez@influenster.com>
2019-08-16 13:34:28 -0300

    Update favicon and meta tags

--- src/components/WhoAmI.js
+++ src/components/WhoAmI.js
@@ -11,7 +11,7 @@ const WhoAmI = (props) => {
             <h2 className="section__title">WHO AM I?</h2>
 
             <p>
-              <strong>Hi I'm Pablo Suarez</strong> a experienced developer, 9 years of commercial
+              <strong>Hi I'm Pablo Suarez</strong> a experienced developer, 8 years of commercial
               experience working with clients from around the world 🌎,
               providing responsive front-end development and back-end integrations.
               <br/>
